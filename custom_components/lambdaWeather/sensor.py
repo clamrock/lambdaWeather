64564f5a24dfb575e0bdb96253dc69fc29680bd1
clamrock <chris.lamrock@gmail.com>
2022-07-08 10:43:34 -0400

    test 3

--- custom_components/lambdaWeather/sensor.py
+++ custom_components/lambdaWeather/sensor.py
@@ -12,26 +12,23 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 
-def setup_platform(
-    hass: HomeAssistant,
-    config: ConfigType,
-    add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None
-) -> None:
+def setup_platform( hass: HomeAssistant, config: ConfigType, add_entities: AddEntitiesCallback, discovery_info: DiscoveryInfoType | None = None) -> None:
     """Set up the sensor platform."""
-    add_entities([ExampleSensor()])
+    name="HereItIs"
+    add_entities([lambdaWeatherSensor(name)])
 
 
-class ExampleSensor(SensorEntity):
+class lambdaWeatherSensor(SensorEntity):
     """Representation of a Sensor."""
+    def __init__(self, _name):
 
-    _attr_name = "Example Temperature"
-    _attr_native_unit_of_measurement = TEMP_CELSIUS
-    _attr_device_class = SensorDeviceClass.TEMPERATURE
-    _attr_state_class = SensorStateClass.MEASUREMENT
+        self._attr_name = _name
+        self._attr_native_unit_of_measurement = TEMP_CELSIUS
+        self._attr_device_class = SensorDeviceClass.TEMPERATURE
+        self._attr_state_class = SensorStateClass.MEASUREMENT
 
     def update(self) -> None:
         """Fetch new state data for the sensor.
         This is the only method that should fetch new data for Home Assistant.
         """
-        self._attr_native_value = 50
+        self._attr_native_value = 20

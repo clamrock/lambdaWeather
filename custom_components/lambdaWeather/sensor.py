8268a6aa687aad9636d52e7dd477da5a5f9efc02
clamrock <chris.lamrock@gmail.com>
2022-07-15 16:33:26 -0400

    Wrapped all into sensor

--- custom_components/lambdaWeather/sensor.py
+++ custom_components/lambdaWeather/sensor.py
@@ -11,7 +11,10 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 import random
-import nws
+
+import asyncio
+import aiohttp
+import pynws
 
 def setup_platform( hass: HomeAssistant, config: ConfigType, add_entities: AddEntitiesCallback, discovery_info: DiscoveryInfoType | None = None) -> None:
     """Set up the sensor platform."""
@@ -22,6 +25,40 @@ def setup_platform( hass: HomeAssistant, config: ConfigType, add_entities: AddEn
     
     add_entities(sensors)
 
+
+def ctof(temp):
+    return ((temp * (9 / 5)) + 32)
+
+
+def calculateApparent(temp, heat, wind):
+    if ctof(temp) > 80:
+        return ctof(heat)
+    if ctof(temp) < 51:
+        return ctof(wind)
+    return ctof(temp)
+
+
+async def example():
+    async with aiohttp.ClientSession() as session:
+        nws = pynws.SimpleNWS(*DETROIT, USERID, session)
+        await
+        nws.set_station()
+        await
+        nws.update_observation()
+        # await nws.update_forecast()
+        # await nws.update_alerts_forecast_zone()
+        # await nws.update_detailed_forecast()
+
+        current_temp = nws.observation['temperature']
+        current_heatIndex = nws.observation['heatIndex']
+        current_windChill = nws.observation['windChill']
+
+        # print("{}, {}, {}".format(current_temp, current_heatIndex, current_windChill))
+
+        apparentTemp = calculateApparent(current_temp, current_heatIndex, current_windChill)
+
+        # print("Apparent temp = {}".format(apparentTemp))
+        return apparentTemp
 
 class lambdaWeatherSensor(SensorEntity):
     """Representation of a Sensor."""

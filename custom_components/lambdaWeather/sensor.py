644a7e06df6e5a85e65b944afa600705a81d940f
clamrock <93685653+clamrock@users.noreply.github.com>
2022-07-15 16:21:39 -0400

    Update sensor.py

--- custom_components/lambdaWeather/sensor.py
+++ custom_components/lambdaWeather/sensor.py
@@ -11,7 +11,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 import random
-import nws
+from nws import *
 
 def setup_platform( hass: HomeAssistant, config: ConfigType, add_entities: AddEntitiesCallback, discovery_info: DiscoveryInfoType | None = None) -> None:
     """Set up the sensor platform."""
